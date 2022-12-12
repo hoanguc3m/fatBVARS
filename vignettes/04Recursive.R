9699b3328e7bee1fcb9716f7633f29deecad78a3
hoanguc3m <nhoang.neu@gmail.com>
2022-12-12 18:49:30 +0100

    Add empirical

--- vignettes/04Recursive.R
+++ vignettes/04Recursive.R
@@ -2,38 +2,34 @@
 ######################################################################
 # Non-parsed command-line arguments
 ######################################################################
-# install.packages("~/Downloads/fatBVARS_0.2.0.tar.gz", repos = NULL, type = "source",
-#                  lib ="/pfs/stor10/users/home/h/hoangoru/Public/R-packages-4.0")
 
-args <- commandArgs(trailingOnly = TRUE)
-print(args)
+# args <- commandArgs(trailingOnly = TRUE)
+# print(args)
 
 library("fatBVARS")
-
-
-#data(package = "fatBVARS", "MonthlyData")
-#datafin = MonthlyData
-#data_name = "MonthlyData"
-
-#length(ts(start = c(2007,1), end = c(2019,12), frequency = 12))
-#length(445:588)
-load("/home/hoanguc3m/Dropbox/WP5/Code/RData/MonthlyData/MonthlyData.RData")
+data(MonthlyData)
+View(datafin)
 data_name = "MonthlyData"
 Time <- as.Date(Monthly$sasdate[133:732], format = "%m/%d/%Y")
 
 
-dist = args[1]
-SV = as.logical(args[2])
-t_start = as.numeric(args[3])
-path = args[4]
-if (is.na(path)) path = "/proj/nobackup/hoanguc3m/recursive/"
+# dist = args[1]
+# SV = as.logical(args[2])
+# t_start = as.numeric(args[3])
+# path = args[4]
+# if (is.na(path)) path = "/proj/nobackup/hoanguc3m/recursive/"
+
+dist = "Gaussian"
+SV = FALSE
+t_start = 500
+path = ""
 
 K = ncol(datafin)
 p = 4
 
 cat(paste(path, data_name, "_", dist, "_", SV, "_", t_start+1, ".RData", sep = ""))
 
-RhpcBLASctl::blas_set_num_threads(1)
+# RhpcBLASctl::blas_set_num_threads(1)
 
 out_recursive <- recursive_seperate(y = datafin, t_start = t_start,
                                     t_pred = 12,
